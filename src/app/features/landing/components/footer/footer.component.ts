767a59e39429225814687049fd2f7d20db305547
nav2510 <singh.navdeep2510@gmail.com>
2021-07-14 02:56:15 +0530

    Fixed minor bugs

--- src/app/features/landing/components/footer/footer.component.ts
+++ src/app/features/landing/components/footer/footer.component.ts
@@ -1,36 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { FOOTTER_LINKS, SOCIAL_LINKS } from './footer.mock';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
   emailControl: FormControl;
-  footerLinks = [
-    [
-      { label: '+91 444 444 4444' },
-      { label: 'singh.navdeep2510@gmail.com' },
-      { label: 'find a store' },
-    ],
-    [
-      { label: 'contact us' },
-      { label: 'ordering and payment' },
-      { label: 'faqs' },
-    ],
-    [
-      { label: 'about interviewdream' },
-      { label: 'work with us' },
-      { label: 'privacy policy' },
-      { label: 'terms & conditions' },
-      { label: 'faqs' },
-    ],
-  ];
-  socialLinks = [
-    { icon: 'instagram', url: '' },
-    { icon: 'linkedin', url: '' },
-    { icon: 'github', url: '' },
-    { icon: 'google-plus', url: '' },
-  ];
+  footerLinks = FOOTTER_LINKS;
+  socialLinks = SOCIAL_LINKS;
+
+  ngOnInit(): void {
+    this.emailControl = new FormControl('');
+  }
 }
