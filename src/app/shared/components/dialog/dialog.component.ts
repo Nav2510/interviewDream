1c2fab99815f7dcd656a1c51160ee46c4d348fa8
nav2510 <singh.navdeep2510@gmail.com>
2021-09-11 16:35:27 +0530

    1. Added file upload functionality 2. Added drag n drop

--- src/app/shared/components/dialog/dialog.component.ts
+++ src/app/shared/components/dialog/dialog.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 import { DialogData } from './models/dialog-data.model';
+import { DialogService } from './services/dialog.service';
 
 @Component({
   selector: 'app-dialog',
@@ -10,16 +11,18 @@ import { DialogData } from './models/dialog-data.model';
 })
 export class DialogComponent<T> {
   constructor(
-    public dialogRef: MatDialogRef<DialogComponent<T>>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData<T>,
+    private readonly dialogService: DialogService,
+    private readonly dialogRef: MatDialogRef<DialogComponent<T>>,
+    @Inject(MAT_DIALOG_DATA) readonly data: DialogData<T>,
   ) {}
 
   onSubmit(): void {
-    this.data.submitMethod();
+    this.dialogService.submit();
     this.dialogRef.close();
   }
 
   onCancel(): void {
+    this.dialogService.cancel();
     this.dialogRef.close();
   }
 }
--- src/app/shared/components/dialog/models/dialog-data.model.ts
+++ src/app/shared/components/dialog/models/dialog-data.model.ts
@@ -3,10 +3,8 @@ import { TemplateRef } from '@angular/core';
 export interface DialogData<T> {
   title: string;
   showCloseBtn: boolean;
+  showFooter: boolean;
   submitLabel: string;
   cancelLabel: string;
-  submitMethod?: () => any;
-  cancelMethod?: () => any;
-  content?: string;
   template?: TemplateRef<T>;
 }
